1cdcda654cac8384d114d69b1a9bf6ce5cf6a705
Johan <johan.boscher@gmail.com>
2020-06-22 18:00:52 +0200

    Working on setting up GUI

--- GUI.py
+++ GUI.py
@@ -1,17 +1,1 @@
-import kivy
-from kivy.app import App
-from kivy.uix.label import Label
-
-#The Base class of our App should always inherit from
-#the App class.
-
-class GebApp(App):
-    def build(self):
-        #This is where we should initialize and
-        #return the root widget:
-        return Label(text='Hello world')
-
-
-
-if __name__=="__main__":
-    GebApp().run()
+from PyQt5.QtWidgets import QApplication, QLabel
